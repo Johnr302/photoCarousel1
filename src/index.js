8d19da7dc7859856306a637ffb8ce5019eebd140
Pjohn <50125720+Johnr302@users.noreply.github.com>
2022-01-14 18:49:14 -0800

    Update index.js
    changed selectedImg to thumbnailElements, created a function to set the correct image.

--- src/index.js
+++ src/index.js
@@ -1,23 +1,27 @@
 import { images } from "../images/images";
 
 const displayImg = document.getElementById("display");
-const selectedImg = document.querySelectorAll(".pictures img");
+const thumbnailElements = document.querySelectorAll(".pictures img");
 const leftArrow = document.getElementById("leftArrow");
 const rightArrow = document.getElementById("rightArrow");
 
 let selectedTargetID = 1;
 
+const setImageSrc = (number) => {
+  return images[selectedTargetID - number];
+};
+
 const loadImages = () => {
-  for (let i = 0; i < selectedImg.length; i++) {
-    selectedImg[i].src = images[i];
+  for (let i = 0; i < thumbnailElements.length; i++) {
+    thumbnailElements[i].src = images[i];
   }
-  displayImg.src = images[0];
+  displayImg.src = setImageSrc(1);
 };
 
 loadImages();
 
 const activeImage = () => {
-  for (const element of selectedImg) {
+  for (const element of thumbnailElements) {
     if (Number(element.id) === selectedTargetID) {
       element.classList.add("activeImg");
       element.parentElement.classList.add("activeBg");
@@ -29,39 +33,37 @@ activeImage();
 
 leftArrow.onclick = () => {
   removeClass();
-  let index = selectedTargetID - 1;
-  displayImg.src = images[index - 1];
+  displayImg.src = setImageSrc(2);
   selectedTargetID--;
   if (selectedTargetID === 0) {
     selectedTargetID = 3;
-    displayImg.src = images[selectedTargetID - 1];
+    displayImg.src = setImageSrc(1);
   }
   activeImage();
 };
 
 rightArrow.onclick = () => {
   removeClass();
-  let index = selectedTargetID - 1;
-  displayImg.src = images[index + 1];
+  displayImg.src = setImageSrc(0);
   selectedTargetID++;
   if (selectedTargetID === 4) {
     selectedTargetID = 1;
-    displayImg.src = images[selectedTargetID - 1];
+    displayImg.src = setImageSrc(1);
   }
   activeImage();
 };
 
-for (const element of selectedImg) {
+for (const element of thumbnailElements) {
   element.onclick = (element, selectedTarget) => {
     removeClass();
     selectedTarget = element.currentTarget;
     selectedTargetID = Number(selectedTarget.id);
-    displayImg.src = images[selectedTargetID - 1];
+    displayImg.src = setImageSrc(1);
     activeImage();
   };
 }
 const removeClass = () => {
-  for (const element of selectedImg) {
+  for (const element of thumbnailElements) {
     element.classList.remove("activeImg");
     element.parentElement.classList.remove("activeBg");
   }
