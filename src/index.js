83cf054dbd8a6f8f0bacf332439d59e43410ee2a
Pjohn <50125720+Johnr302@users.noreply.github.com>
2022-01-17 20:04:16 -0800

    Update index.js
    refactor code

--- src/index.js
+++ src/index.js
@@ -1,23 +1,34 @@
 import { images } from "../images/images";
 
 const displayImg = document.getElementById("display");
-const selectedImg = document.querySelectorAll(".pictures img");
+const thumbnailElements = document.querySelectorAll(".pictures img");
 const leftArrow = document.getElementById("leftArrow");
 const rightArrow = document.getElementById("rightArrow");
 
 let selectedTargetID = 1;
 
+const setImageSrc = () => {
+  return images[selectedTargetID - 1];
+};
+
 const loadImages = () => {
-  for (let i = 0; i < selectedImg.length; i++) {
-    selectedImg[i].src = images[i];
+  for (const [index, element] of thumbnailElements.entries()) {
+    element.src = images[index];
+    element.onclick = (element, selectedTarget) => {
+      removeClass();
+      selectedTarget = element.currentTarget;
+      selectedTargetID = Number(selectedTarget.id);
+      displayImg.src = images[index];
+      selectThumbnail();
+    };
   }
-  displayImg.src = images[0];
+  displayImg.src = setImageSrc();
 };
 
 loadImages();
 
-const activeImage = () => {
-  for (const element of selectedImg) {
+const selectThumbnail = () => {
+  for (const element of thumbnailElements) {
     if (Number(element.id) === selectedTargetID) {
       element.classList.add("activeImg");
       element.parentElement.classList.add("activeBg");
@@ -25,44 +36,33 @@ const activeImage = () => {
   }
 };
 
-activeImage();
+selectThumbnail();
 
-leftArrow.onclick = () => {
-  removeClass();
-  let index = selectedTargetID - 1;
-  displayImg.src = images[index - 1];
-  selectedTargetID--;
-  if (selectedTargetID === 0) {
-    selectedTargetID = 3;
-    displayImg.src = images[selectedTargetID - 1];
-  }
-  activeImage();
-};
-
-rightArrow.onclick = () => {
-  removeClass();
-  let index = selectedTargetID - 1;
-  displayImg.src = images[index + 1];
-  selectedTargetID++;
-  if (selectedTargetID === 4) {
-    selectedTargetID = 1;
-    displayImg.src = images[selectedTargetID - 1];
-  }
-  activeImage();
-};
-
-for (const element of selectedImg) {
-  element.onclick = (element, selectedTarget) => {
-    removeClass();
-    selectedTarget = element.currentTarget;
-    selectedTargetID = Number(selectedTarget.id);
-    displayImg.src = images[selectedTargetID - 1];
-    activeImage();
-  };
-}
 const removeClass = () => {
-  for (const element of selectedImg) {
+  for (const element of thumbnailElements) {
     element.classList.remove("activeImg");
     element.parentElement.classList.remove("activeBg");
   }
+};
+
+const arrowHandler = () => {
+  removeClass();
+  if (selectedTargetID === 0) {
+    selectedTargetID = 3;
+  }
+  if (selectedTargetID === 4) {
+    selectedTargetID = 1;
+  }
+  displayImg.src = setImageSrc();
+  selectThumbnail();
+};
+
+leftArrow.onclick = () => {
+  selectedTargetID--;
+  arrowHandler();
+};
+
+rightArrow.onclick = () => {
+  selectedTargetID++;
+  arrowHandler();
 };
